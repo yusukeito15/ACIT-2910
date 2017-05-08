e318011cfab77f56d3a14ce6402ec778d0a34cbf
Alex <alex_christensen92@hotmail.com>
2017-05-08 15:14:18 -0700

    Changes CSS and added the proper changes to the Database for logging in
    Signed-off-by: Alex <alex_christensen92@hotmail.com>

--- build/homebundle.js
+++ build/homebundle.js
@@ -10332,14 +10332,31 @@ return jQuery;
 
 /* WEBPACK VAR INJECTION */(function($) {$(document).ready(function(){
     var login = document.getElementById("login")
+    
     login.addEventListener("click", function(){
         location.href = "/loginPage";
     });
     
-    var profile = document.getElementById("profile")
-    profile.addEventListener("click", function(){
-        location.href = "/profile";
+    $.ajax({
+        url:"/xiEzMyEY6LAhMzQhYS0=",
+        success:function(resp){
+            if(resp.type){
+                document.getElementById("login").style.display = "none";
+                document.getElementById("logout").style.visibility = "visible";
+            }
+        }
     });
+    
+    document.getElementById("logout").addEventListener("click", function(){
+        $.ajax({
+            url:"/logout",
+            type:"post",
+            success:function(resp){
+                location.reload();
+                }
+            })
+    });
+    
 })
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
--- js/profile.js
+++ js/profile.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
-    console.log("Ready!");
     
-    var login = document.getElementById("login")
-    login.addEventListener("click", function(){
-        location.href = "/loginPage";
+    document.getElementById("homeLogo").addEventListener("click", function(){
+        location.href = "/"
     });
     
     $.ajax({
-       url:"/session",
-       success:function(resp){
-           console.log("Email: " + resp.email);
-           document.getElementById("email").innerHTML = resp.email;
-       }
+        url:"/xiEzMyEY6LAhMzQhYS0=",
+        success:function(resp){
+            
+            document.getElementById("email").innerHTML = resp.email; 
+        }
     });
-})
+    
+    $.ajax({
+        url:"/xiEzMyEY6LAhMzQhYS0=",
+        success:function(resp){
+            if(resp.type){
+                document.getElementById("login").style.display = "none";
+                document.getElementById("logout").style.visibility = "visible";
+            }
+        }
+    });
+    
+    document.getElementById("logout").addEventListener("click", function(){
+        $.ajax({
+            url:"/logout",
+            type:"post",
+            success:function(resp){
+                location.reload();
+                }
+            })
+    });
+    
+});

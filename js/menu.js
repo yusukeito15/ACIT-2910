d26f5925be2d51566f4c6f17e7e9bd203020ad6c
Alex <alex_christensen92@hotmail.com>
2017-05-10 11:04:16 -0700

    refactored the ordering so it all runs just off one function #CodeSmarter
    Signed-off-by: Alex <alex_christensen92@hotmail.com>

--- js/menu.js
+++ js/menu.js
@@ -38,15 +38,7 @@ $(document).ready(function(){
         success:function(resp){
             
             for(var i = 0; i<resp.length; i++){
-                var ndiv = document.createElement("div");
-                ndiv.className = "menuDiv";
-                ndiv.innerHTML = resp[i].itemname;
-                var nImg = document.createElement("img");
-                nImg.className = "menuImg";
-                nImg.src = "/fp/"+resp[i].picture;
-                ndiv.appendChild(nImg);
-                document.getElementById("leftContainer").appendChild(ndiv);
-                
+               
                 if(resp[i].type == "main"){
                     main.push(resp[i]);
                 }else if(resp[i].type == "sides") {
@@ -58,16 +50,57 @@ $(document).ready(function(){
                 } else {
                     console.log("Okay something went very very wrong");
                 }
+            }
+        }
+
+    });
+    
+    document.getElementById("mainFood").addEventListener("click", function(){
+        document.getElementById("leftContainer").innerHTML = "";
+        menuMaker(main);
+
+    });
+    
+     document.getElementById("sideFood").addEventListener("click", function(){
+        document.getElementById("leftContainer").innerHTML = "";        
+        menuMaker(side);
+         
+    });
+    
+    document.getElementById("dessFood").addEventListener("click", function(){
+        document.getElementById("leftContainer").innerHTML = "";
+        menuMaker(dessert);
+
+    });
+    
+    document.getElementById("brevFood").addEventListener("click", function(){
+        document.getElementById("leftContainer").innerHTML = "";
+        menuMaker(beverage);
+        
+    });
+    
+    //function to make the menu's appear when you click on a tab at the top, just have to change styling
+    //on one of these to change them all!
+    
+    function menuMaker(menutype){
+                for(var i = 0; i<menutype.length; i++){
+                var ndiv = document.createElement("div");
+                ndiv.className = "menuDiv";
+                ndiv.innerHTML = menutype[i].itemname;
+                var nImg = document.createElement("img");
+                nImg.className = "menuImg";
+                nImg.src = "/fp/"+menutype[i].picture;
+                ndiv.appendChild(nImg);
+                document.getElementById("leftContainer").appendChild(ndiv);
                 
-                ndiv.title = resp[i].itemname;
-                ndiv.price = resp[i].price;
-                ndiv.desc = resp[i].description;
-                ndiv.pic = resp[i].picture;
+                ndiv.title = menutype[i].itemname;
+                ndiv.price = menutype[i].price;
+                ndiv.desc = menutype[i].description;
+                ndiv.pic = menutype[i].picture;
                 
                 ndiv.addEventListener("click", function(){
                     var orderButton = document.createElement("button");
                     orderButton.innerHTML = "ORDER NOW";
-                    var ndiv = document.createElement("div");
                     document.getElementById("testInfo").style.opacity = "1";
                     var nnImg = document.createElement("img");
                     nnImg.className = "menuImg";
@@ -76,7 +109,7 @@ $(document).ready(function(){
                     document.getElementById("testInfo").appendChild(nnImg);
                     document.getElementById("testInfo").appendChild(orderButton);
                     console.log("working!");
-                    
+                                        
                     ndiv.title = this.title;
                     ndiv.price = this.price;
                     
@@ -100,143 +133,9 @@ $(document).ready(function(){
                         })
                     });
                 });
-            }
-        }
-    });
-    
-    document.getElementById("mainFood").addEventListener("click", function(){
-        document.getElementById("leftContainer").innerHTML = "";
+        };
         
-        for(var i = 0; i<main.length; i++){
-                var ndiv = document.createElement("div");
-                ndiv.className = "menuDiv";
-                ndiv.innerHTML = main[i].itemname;
-                var nImg = document.createElement("img");
-                nImg.className = "menuImg";
-                nImg.src = "/fp/"+main[i].picture;
-                ndiv.appendChild(nImg);
-                document.getElementById("leftContainer").appendChild(ndiv);
-                
-                ndiv.title = main[i].itemname;
-                ndiv.price = main[i].price;
-                ndiv.desc = main[i].description;
-                ndiv.pic = main[i].picture;
-                
-                ndiv.addEventListener("click", function(){
-                    var orderButton = document.createElement("button");
-                    orderButton.innerHTML = "ORDER NOW";
-                    document.getElementById("testInfo").style.opacity = "1";
-                    var nnImg = document.createElement("img");
-                    nnImg.className = "menuImg";
-                    nnImg.src = "/fp/"+this.pic;
-                    document.getElementById("testInfo").innerHTML = this.title + "<br>$"+this.price+ ",  "+ this.desc;
-                    document.getElementById("testInfo").appendChild(nnImg);
-                    document.getElementById("testInfo").appendChild(orderButton);
-                    console.log("working!");
-                });
-        }
-    });
-    
-     document.getElementById("sideFood").addEventListener("click", function(){
-        document.getElementById("leftContainer").innerHTML = "";
-        
-        for(var i = 0; i<side.length; i++){
-                var ndiv = document.createElement("div");
-                ndiv.className = "menuDiv";
-                ndiv.innerHTML = side[i].itemname;
-                var nImg = document.createElement("img");
-                nImg.className = "menuImg";
-                nImg.src = "/fp/"+side[i].picture;
-                ndiv.appendChild(nImg);
-                document.getElementById("leftContainer").appendChild(ndiv);
-                
-                ndiv.title = side[i].itemname;
-                ndiv.price = side[i].price;
-                ndiv.desc = side[i].description;
-                ndiv.pic = side[i].picture;
-                
-                ndiv.addEventListener("click", function(){
-                    var orderButton = document.createElement("button");
-                    orderButton.innerHTML = "ORDER NOW";
-                    document.getElementById("testInfo").style.opacity = "1";
-                    var nnImg = document.createElement("img");
-                    nnImg.className = "menuImg";
-                    nnImg.src = "/fp/"+this.pic;
-                    document.getElementById("testInfo").innerHTML = this.title + "<br>$"+this.price+ ",  "+ this.desc;
-                    document.getElementById("testInfo").appendChild(nnImg);
-                    document.getElementById("testInfo").appendChild(orderButton);
-                    console.log("working!");
-                });
-        }
-    });
-    
-    document.getElementById("dessFood").addEventListener("click", function(){
-        document.getElementById("leftContainer").innerHTML = "";
-        
-        for(var i = 0; i<dessert.length; i++){
-                var ndiv = document.createElement("div");
-                ndiv.className = "menuDiv";
-                ndiv.innerHTML = dessert[i].itemname;
-                var nImg = document.createElement("img");
-                nImg.className = "menuImg";
-                nImg.src = "/fp/"+dessert[i].picture;
-                ndiv.appendChild(nImg);
-                document.getElementById("leftContainer").appendChild(ndiv);
-                
-                ndiv.title = dessert[i].itemname;
-                ndiv.price = dessert[i].price;
-                ndiv.desc = dessert[i].description;
-                ndiv.pic = dessert[i].picture;
-                
-                ndiv.addEventListener("click", function(){
-                    var orderButton = document.createElement("button");
-                    orderButton.innerHTML = "ORDER NOW";
-                    document.getElementById("testInfo").style.opacity = "1";
-                    var nnImg = document.createElement("img");
-                    nnImg.className = "menuImg";
-                    nnImg.src = "/fp/"+this.pic;
-                    document.getElementById("testInfo").innerHTML = this.title + "<br>$"+this.price+ ",  "+ this.desc;
-                    document.getElementById("testInfo").appendChild(nnImg);
-                    document.getElementById("testInfo").appendChild(orderButton);
-                    console.log("working!");
-                });
-        }
-    });
-    
-    document.getElementById("brevFood").addEventListener("click", function(){
-        document.getElementById("leftContainer").innerHTML = "";
-        
-        for(var i = 0; i<beverage.length; i++){
-                var ndiv = document.createElement("div");
-                ndiv.className = "menuDiv";
-                ndiv.innerHTML = beverage[i].itemname;
-                var nImg = document.createElement("img");
-                nImg.className = "menuImg";
-                nImg.src = "/fp/"+beverage[i].picture;
-                ndiv.appendChild(nImg);
-                document.getElementById("leftContainer").appendChild(ndiv);
-                
-                ndiv.title = beverage[i].itemname;
-                ndiv.price = beverage[i].price;
-                ndiv.desc = beverage[i].description;
-                ndiv.pic = beverage[i].picture;
-                
-                ndiv.addEventListener("click", function(){
-                    var orderButton = document.createElement("button");
-                    orderButton.innerHTML = "ORDER NOW";
-                    document.getElementById("testInfo").style.opacity = "1";
-                    var nnImg = document.createElement("img");
-                    nnImg.className = "menuImg";
-                    nnImg.src = "/fp/"+this.pic;
-                    document.getElementById("testInfo").innerHTML = this.title + "<br>$"+this.price+ ",  "+ this.desc;
-                    document.getElementById("testInfo").appendChild(nnImg);
-                    document.getElementById("testInfo").appendChild(orderButton);
-                    console.log("working!");
-                });
-        }
-    });
-    
-
+    };
 
 });
 
